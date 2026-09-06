5186f2b19d648047704111a9b62ed10013c49681
mohamed ellithy <m.elsayed@tildetech.ae>
2026-09-06 09:43:44 +0300

    Add an Entities

---
+++ Talabat.Core/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Talabat.Core.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal Price { get; set; }
+        public int productBrandId { get; set; }
+        public int productTypeId { get; set; }
+        public ProductBrand ProductBrand { get; set; }
+        public ProductType ProductType { get; set; }
+    }
+}
---
+++ Talabat.Core/Entities/ProductType.cs
@@ -0,0 +1,8 @@
+﻿namespace Talabat.Core.Entities
+{
+    public class ProductType
+    {
+        public string Name { get; set; }
+
+    }
+}
